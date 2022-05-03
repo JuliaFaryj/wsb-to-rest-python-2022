a0cef0f61db7232eabc7ec3a65cc61791e3ac8e0
TatZhuk <tzhukauskene@gmail.com>
2022-05-03 17:51:54 +0200

    update test1 and test2

--- tests/test2-list-all-branches-from-repository.py
+++ tests/test2-list-all-branches-from-repository.py
@@ -1,5 +1,4 @@
 import requests
 
 def test_list_all_branches_():
-    r = requests.get ('https://api.github.com/TatZhuk/wsb-to-rest-python-2022/branches, auth=('user', 'pass'))
-    r.status_code == 200
+    r = requests

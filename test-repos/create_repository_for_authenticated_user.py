69a3690f060d8c5d5e5cd8309c461b60be229b8d
Julia Faryj <juliafaryj@gmail.com>
2022-06-07 19:50:28 +0200

    added test_status_201_for_new_private_repo

--- test-repos/create_repository_for_authenticated_user.py
+++ test-repos/create_repository_for_authenticated_user.py
@@ -1,10 +1,12 @@
 # https://docs.github.com/en/rest/repos/repos#create-a-repository-for-the-authenticated-user
 # tests for authenticated user to create a new repository
 
+
 import requests
 from settings.credentials import GITHUB_API_USER
 from settings.credentials import GITHUB_API_TOKEN
 
+# tests for authenticated user to create a new public repository
 def test_status_201_for_new_repo():
     restUrl = "https://api.github.com/user/repos"
     dataToSend = {
@@ -17,6 +19,21 @@ def test_status_201_for_new_repo():
 
     assert response.status_code == 201
 
+# tests for authenticated user to create a new private repository
+def test_status_201_for_new_private_repo():
+    restUrl = "https://api.github.com/user/repos"
+    dataToSend = {
+        "name": "my-python-created-test-private-repo-01",
+        "private": True
+    }
+    response = requests.post(
+            url=restUrl,
+            auth= (GITHUB_API_USER, GITHUB_API_TOKEN),
+            json=dataToSend)
+
+    assert response.status_code == 201
+
+# tests for authenticated user to create repository with name already existing
 def test_status_422_when_creating_repo_with_name_already_existing():
     restUrl = "https://api.github.com/user/repos"
     dataToSend = {
@@ -28,3 +45,4 @@ def test_status_422_when_creating_repo_with_name_already_existing():
             json=dataToSend)
 
     assert response.status_code == 422
+

f8aa7fdbeec46257891f9e434f2d3988643db943
Julia Faryj <juliafaryj@gmail.com>
2022-06-12 16:27:25 +0200

    added test for existing public repo

--- test-repos/get_repository.py
+++ test-repos/get_repository.py
@@ -14,5 +14,12 @@ def test_status_200_for_existing_private_repo():
     assert jsonRepoInfo["name"] == "my-python-created-test-private-repo-01"
     assert jsonRepoInfo["private"] == True
 
-
+def test_status_200_for_existing_public_repo():
+    url = "https://api.github.com/repos/JuliaFaryj/my-python-created-test-public-repo-01"
+    # send GET request without credentials (don't need credentials, because requested repo (my-python-created-test-public-repo-01) ist public)
+    response = requests.get(url)
+    assert response.status_code == 200
+    jsonRepoInfo = response.json()
+    assert jsonRepoInfo["name"] == "my-python-created-test-public-repo-01"
+    assert jsonRepoInfo["private"] == False
 

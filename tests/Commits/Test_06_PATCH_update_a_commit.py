90a0997f84da6c99921c8c218a494c133770c8cf
Aleksandra Wszeborowska <105010486+AleksandraWszeborowska@users.noreply.github.com>
2022-06-19 01:36:43 +0300

    Dodanie dzialajacego testu delete

--- tests/Commits/Test_06_PATCH_update_a_commit.py
+++ tests/Commits/Test_06_PATCH_update_a_commit.py
@@ -5,9 +5,9 @@ import requests
 #Test using PATCH
 def test_update_commit_comment():
     headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
-    data = {"body": "To jest aktualizacja nr 1 komentarza1."}
+    data = {"body": "To jest aktualizacja nr 1 pierwszego komentarza."}
 
-    url = url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments/76208177"
+    url = url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments/75631994"
     response = requests.patch(url, headers=headers, json=data)
     assert response.status_code == 200
 
--- tests/Commits/Test_07_DELETE_a_commit_comment.py
+++ tests/Commits/Test_07_DELETE_a_commit_comment.py
@@ -1,11 +1,62 @@
+import requests
 from settings import TOKEN
 
-import requests
+
+# Test using POST
+def test_create_commit_comment():
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
+    data = {
+        "body": "New comment is done!",
+        "path": "GETcommits/lista.txt"
+    }
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/commits/fcdf6e3112e594e250481d20eb5c9203d818dbc0/comments"
+    response = requests.post(url, headers=headers, json=data)
+    assert response.status_code == 201
+    delete_commit_comment()
+
+
+def get_comment_id():
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments"
+    response = requests.get(url, headers=headers)
+    response_body = response.json()
+    comment_id = response_body[22]["id"]
+    return comment_id
+
+
+def delete_commit_comment():
+    comment_id = get_comment_id()
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments/" + str(comment_id)
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 204
 
 #Test using DELETE
-def test_delete_commit_comment():
+
+
+def create_commit_comment():
     headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
-    data = {"body": "To jest aktualizacja nr 1 komentarza1."}
-    url = url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments/" + comment_id
-    response = requests.delete(url, headers=headers, json=data)
+    data = {
+        "body": "It is next comment",
+        "path": "GETcommits/lista.txt"
+    }
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/commits/fcdf6e3112e594e250481d20eb5c9203d818dbc0/comments"
+    requests.post(url, headers=headers, json=data)
+
+
+def get_comment_id():
+    create_commit_comment()
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments"
+    response = requests.get(url, headers=headers)
+    response_body = response.json()
+    comment_id = response_body[22]["id"]
+    return comment_id
+
+
+def test_delete_commit_comment():
+    comment_id = get_comment_id()
+    headers = {"Accept": "application/vnd.github.v3+json", "Authorization": "token " + TOKEN}
+    url = "https://api.github.com/repos/AleksandraWszeborowska/Windows/comments/" + str(comment_id)
+    response = requests.delete(url, headers=headers)
     assert response.status_code == 204

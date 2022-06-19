7b0021c1e054a5f971b74b41b4ed4dcecd753ca1
Julia Faryj <juliafaryj@gmail.com>
2022-06-19 10:31:12 +0200

    added authentication to fix issue with API limits exceeded allowed value for unauthenticated user

--- tests/repos/test_60_list_repos_for_a_user.py
+++ tests/repos/test_60_list_repos_for_a_user.py
@@ -1,21 +1,22 @@
-# https://docs.github.com/en/rest/repos/repos#list-repositories-for-a-user
+# c
 # tests for unauthenticated user to list public repos of other user
 import requests
 from settings.credentials import GITHUB_API_USER
+from settings.credentials import GITHUB_API_TOKEN
 
 def test_status_404_for_not_existing_user():
     url = "https://api.github.com/users/USERNAME_not_exists/repos"
-    response = requests.get(url)
+    response = requests.get(url, auth=(GITHUB_API_USER, GITHUB_API_TOKEN))
     assert response.status_code == 404
 
 def test_status_200_for_existing_user():
     url = "https://api.github.com/users/"+GITHUB_API_USER+"/repos"
-    response = requests.get(url)
+    response = requests.get(url, auth=(GITHUB_API_USER, GITHUB_API_TOKEN))
     assert response.status_code == 200
 
 def test_public_repo_exist():
     url = "https://api.github.com/users/"+GITHUB_API_USER+"/repos"
-    response = requests.get(url)
+    response = requests.get(url, auth=(GITHUB_API_USER, GITHUB_API_TOKEN))
     assert response.status_code == 200
     jsonBody = response.json()
     repoExists = False
